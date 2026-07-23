6271b349c6d4171a24e1fdb30183a68f274806c7
Zarifa Babayeva <zerifexasmemmedli@gmail.com>
2026-07-23 23:16:37 +0400

    Digər tapşırıqlar silindi

--- src/Polymorphism/EmailBildiris.java
+++
@@ -1,7 +0,0 @@
-package Polymorphism;
-public class EmailBildiris extends Bildiris {
-    @Override
-    public void gonder(String mesaj) {
-        System.out.println("[E-mail]: '" + mesaj + "' - mətni poçt ünvanına göndərildi.");
-    }
-}
--- src/Polymorphism/PushBildiris.java
+++
@@ -1,7 +0,0 @@
-package Polymorphism;
-public class PushBildiris extends Bildiris {
-    @Override
-    public void gonder(String mesaj) {
-        System.out.println("[Push]: '" + mesaj + "' - mətni tətbiq bildirişi kimi ekrana çıxdı.");
-    }
-}
